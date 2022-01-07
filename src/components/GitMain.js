a4fd288cd55d932d49cc8f1a646b8956b5146bf7
Leonardo <tteixeira.leonardo@gmail.com>
2022-01-07 19:32:45 -0300

    feat(new component): cria o componente gituserrepos
    cria o componente gituserrespos populado com os dados obtidos pela api do github
    
    issue #13

--- src/components/GitMain.js
+++ src/components/GitMain.js
@@ -6,16 +6,19 @@ import { Main } from '@Styles/main';
 // COMPONENTS
 import GitSearch from '@Components/GitSearch';
 import GitUserBoard from '@Components/GitUserBoard';
+import GitUserRepos from '@Components/GitUserRepos';
 
 // UTILS
 import { getUserData } from '@Utils/api';
 
 const GitMain = () => {
   const [userData, setUserData] = useState(null);
+  const [showRepos, setShowRepos] = useState(false);
 
   const buildUserProfile = (username) => {
     getUserData(username).then((data) => {
       setUserData(data);
+      setShowRepos(false);
     });
   };
 
@@ -23,6 +26,13 @@ const GitMain = () => {
     <Main>
       <GitSearch buildUserProfile={buildUserProfile} />
       {userData && <GitUserBoard userProfile={userData.profile} />}
+      {userData && (
+        <GitUserRepos
+          reposList={userData.repos}
+          showRepos={showRepos}
+          setShowRepos={setShowRepos}
+        />
+      )}
     </Main>
   );
 };
---
+++ src/components/GitUserRepos.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+// THEME
+import theme from '@Styles/theme';
+
+// ICONS
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { FaRegHandPointer } from 'react-icons/fa';
+
+// STYLES
+import {
+  RepoContainer,
+  RepoButton,
+  RepoText,
+  RepoCardsList,
+  RepoCard,
+  RepoTitle,
+  RepoLink,
+  RepoWrapper,
+  RepoExtra,
+} from '@Styles/main';
+
+// UTILS
+import { nanoid } from 'nanoid';
+import { formatDate } from '@Utils/api';
+
+const GitUserRepos = ({ reposList, showRepos, setShowRepos }) => {
+  const handleShowRepos = () => {
+    setShowRepos(!showRepos);
+  };
+
+  return (
+    <RepoContainer>
+      <ThemeProvider theme={theme}>
+        {!showRepos && (
+          <RepoButton onClick={() => handleShowRepos()}>Show Repos</RepoButton>
+        )}
+      </ThemeProvider>
+      {showRepos && (
+        <RepoCardsList>
+          {reposList.map((repo) => {
+            const {
+              html_url,
+              name,
+              description,
+              stargazers_count,
+              updated_at,
+            } = repo;
+            return (
+              <RepoCard key={`repo-${nanoid()}`}>
+                <RepoTitle>
+                  <RepoLink href={html_url} target='_blank'>
+                    {name}
+                  </RepoLink>
+                  <FaRegHandPointer />
+                </RepoTitle>
+                <RepoText>
+                  {description ? description : 'No description'}
+                </RepoText>
+                <RepoWrapper>
+                  <RepoExtra>
+                    <FaRegStar />
+                    {stargazers_count}
+                  </RepoExtra>
+                  <GoPrimitiveDot size={10} />
+                  <RepoExtra>Updated at {formatDate(updated_at)}</RepoExtra>
+                </RepoWrapper>
+              </RepoCard>
+            );
+          })}
+        </RepoCardsList>
+      )}
+    </RepoContainer>
+  );
+};
+
+export default GitUserRepos;
